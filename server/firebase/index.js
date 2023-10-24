6ba94bee09be3925c7510684094e115b21990ffb
ONP <74316362+ONP4AM@users.noreply.github.com>
2023-10-24 09:43:27 +0530

    Update index.js

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -4,7 +4,7 @@ var serviceAccount = require("../config/firbaseServiceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  //databaseURL:"https://eshop-dc739.firebaseio.com"
+  //databaseURL:"https://*.firebaseio.com"
 });
 
 
